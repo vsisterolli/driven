2f891e81e9c490e7f35d58e17e7f9459badbc007
vsisterolli <vsisterolli@gmail.com>
2022-08-11 21:56:29 -0300

    Finished confirm action, layout bug solved

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -1,7 +1,8 @@
 let selected_items = 0;
+let selected = [];
 let isSelected = [];
 let cart = [["teste", 1.5], ["teste2", 2.5], ["teste3", 3.3]];
-    
+
 
 function send_request() {
 
@@ -23,7 +24,7 @@ function send_request() {
 }
 
 function confirm() {
-    
+
     const hide = document.querySelectorAll('.header, .page, .footer');
     for(let i = 0; i < hide.length; i++)
         hide[i].style.setProperty('opacity', '5%')
@@ -41,13 +42,19 @@ function cancel() {
         hide[i].style.setProperty('opacity', '100%')
 
     const confirm_box = document.querySelector('.confirm');
+    confirm_box.style.setProperty('display', 'none', 'important');     
+}
+
+function selecionar(index) {
+ 
+    const confirm_box = document.querySelector('.confirm');
     confirm_box.style.setProperty('display', 'none', 'important'); 
-    
+
 }
 
 function allow_order() {
 
-    const button = document.querySelector('button');
+    const button = document.querySelector('.footer button');
     button.textContent = "Fechar pedido";
     button.style.setProperty("background-color", "#32B72F");
     button.setAttribute('onclick', 'confirm()');
@@ -58,12 +65,15 @@ function select(index) {
 
     const section = parseInt(index/7);
 
+    const inicio_secao = parseInt(index/7) * 7;
     const section_start = section * 7;
 
     const items = document.querySelectorAll('.item');
-    
-    for(let i = section_start; i < section_start + 7; i++) {
+
     let imgs = document.querySelectorAll('.order-bottom img')
+
+
+    for(let i = section_start; i < section_start + 7; i++) {
         if(i != index && isSelected[i]) {
             isSelected[i] = 0;
             selected_items--;
@@ -72,8 +82,10 @@ function select(index) {
         }
         else if(!isSelected[i] && i == index) { 
             items[i].style.setProperty("box-shadow", "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F");
+            imgs[i].style.setProperty("display", "initial")
+            selected[index] = 1;
             imgs[i].style.setProperty("display", "initial");
-            
+
             cart[section][0] = document.querySelectorAll('.item h1')[i].textContent
             cart[section][1] = document.querySelectorAll('.preco')[i].textContent;
 
@@ -84,8 +96,8 @@ function select(index) {
             selected_items++;
         }
     }
-    
+
     if(selected_items == 3)
         allow_order();
 
-}
+} 
